59f3ac2492175f56844b87e07cede1d4c1db8681
Agustin Amato <agustin.amato96@gmail.com>
2020-10-18 23:48:31 -0300

    [ruta-notas] rutas para el recurso notas

--- routes/note.js
+++ routes/note.js
@@ -1,17 +1,73 @@
 import express from "express";
-import Nota from "../models/note";
+import Note from "../models/note";
 
 const router = express.Router();
 
-router.post("/note", async (req, res) => {
+router.get("/notes", async (req, res) => {
+  try {
+    const note = await Note.find();
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.post("/notes", async (req, res) => {
   const body = req.body;
 
   try {
-    const note = await Nota.create(body);
-    return res.status(200).json(note);
-
+    const note = await Note.create(body);
+    return res.json(note);
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({ error });
+  }
+});
+
+router.get("/notes/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const note = await Note.findOne({ _id });
+
+    if (!note) {
+      return res.status(500).json({ error: "No se encuentra la nota" });
+    }
+
+    return res.json(note);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+router.put("/notes/:id", async (req, res) => {
+  const _id = req.params.id;
+  const body = req.body;
+
+  try {
+    const note = await Note.findByIdAndUpdate(_id, body, { new: true });
+
+    if (!note) {
+      return res.status(500).json({ error: "No se encuentra la nota" });
+    }
+
+    return res.json(note);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+router.delete("/notes/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const note = await Note.findByIdAndDelete({ _id });
+
+    if (!note) {
+      return res.status(500).json({ error: "No se encuentra la nota" });
+    }
+
+    return res.json(note);
+  } catch (error) {
     return res.status(500).json({ error });
   }
 });
